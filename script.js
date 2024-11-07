9e691ce92f3c7292ceaaf89bb2adffa5f5294227
Jean Figeuroa <160565504+Hasturious@users.noreply.github.com>
2024-11-06 19:06:04 -0800

    Added pop statements

--- script.js
+++ script.js
@@ -31,6 +31,8 @@ console.log(numberArray[4])
 numberArray.push(7, 8, 9, 0)
 /*When this push is declared 7 8 9 0 is added to array and will appear when logged by the function below.
 If the push is declared below the for loop then the array being looped will not have the new pusshe content*/
+numberArray.pop(1, 3, 5)
+//The 1, 3, and 5 values are gone and the array values move accordingly (because 1 is gone 6 becomes the new 0)
 
 for (let i = 0; i < numberArray.length; i++) {
     console.log(numberArray[i])
